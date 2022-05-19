4c03701a4dfe53019e13834370a47e463149f930
dennis-199 <otieno.dennis@strathmore.edu>
2022-05-19 20:49:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/juakali/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.juakali;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+    }
+
+    public void register(View view) {
+    }
+
+    public void Forgotpassword(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/juakali/Splash_screen.java
@@ -0,0 +1,32 @@
+package com.example.juakali;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent welcomeIntent = new Intent(Splash_screen.this, LoginActivity.class);
+                    startActivity(welcomeIntent);
+                }
+
+            }
+
+        };
+        thread.start();
+    }
+}
