11bd332cbc1b65beae2946d9a944713b16792943
dennis-199 <otieno.dennis@strathmore.edu>
2022-05-20 19:04:25 +0300

    categories_update

--- app/src/main/java/com/example/juakali/MainActivity.java
+++ app/src/main/java/com/example/juakali/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.juakali;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -10,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,40 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.navigation);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new HomeFragment()).commit();
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment=null;
+            switch (item.getItemId())
+            {
+                case R.id.home:
+                    fragment=new HomeFragment();
+                    break;
+
+                case R.id.categories:
+                    fragment=new CategoryFragment();
+                    break;
+
+                case R.id.services:
+                    fragment=new ServicesFragment();
+                    break;
+
+                case R.id.trade:
+                    fragment=new TradeFragment();
+                    break;
+
+                case R.id.account:
+                    fragment=new AccountFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
+            return true;
+        }
+    };
 
 }
